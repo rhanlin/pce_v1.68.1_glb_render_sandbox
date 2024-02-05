c8aa9e084511ec04e459548df2d57eb121833b22
rhanlin <spencerlin999@gmail.com>
2024-02-05 22:11:35 +0800

    first commit

---
+++ settings.js
@@ -0,0 +1,15 @@
+export const INPUT_SETTINGS = {
+  useKeyboard: true,
+  useMouse: true,
+  useGamepads: false,
+  useTouch: true,
+};
+export const ASSET_PREFIX = '';
+export const CONFIG_PATH = './config.json';
+export const CONTEXT_OPTIONS = {
+  antialias: true,
+  alpha: true,
+  preserveDrawingBuffer: false,
+  preferWebGl2: true,
+  powerPreference: 'high-performance',
+};
